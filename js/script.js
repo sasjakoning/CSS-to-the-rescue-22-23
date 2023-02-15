e382baeea6532a578f1ea58ad7475deccc9a56ec
Sasja Koning <sasjakoning@hotmail.com>
2023-02-15 12:10:44 +0100

    changed: structure for loader

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 const rotateYSlider = document.getElementById('rotateY');
 document.documentElement.style.setProperty('--rotateY', rotateYSlider.value + 'deg');
 
+const rotateXSlider = document.getElementById('rotateX');
+document.documentElement.style.setProperty('--rotateX', rotateYSlider.value + 'deg');
+
+const translateZSlider = document.getElementById('translateZ');
+document.documentElement.style.setProperty('--translateZ', rotateYSlider.value + 'em');
+
+
 
 // edit the rotateY css variable based on the slider value
 rotateYSlider.addEventListener('input', (e) => {
     document.documentElement.style.setProperty('--rotateY', e.target.value + 'deg');
+    }
+);
+
+// edit the rotateX css variable based on the slider value
+rotateXSlider.addEventListener('input', (e) => {
+    document.documentElement.style.setProperty('--rotateX', e.target.value + 'deg');
+    }
+);
+
+translateZSlider.addEventListener('input', (e) => {
+    document.documentElement.style.setProperty('--translateZ', e.target.value + 'em');
     }
 );
